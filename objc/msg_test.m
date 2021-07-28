84dd142b9b0d544ded3c2ecb262c6fb79af07539
Ethan Reesor <ethan.reesor@gmail.com>
2021-07-28 15:24:29 -0500

    Optimize sendMsg

---
+++ objc/msg_test.m
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+NSNumber * numberWithBool            (BOOL v)               { return [NSNumber numberWithBool:v];             }
+NSNumber * numberWithChar            (signed char v)        { return [NSNumber numberWithChar:v];             }
+NSNumber * numberWithDouble          (double v)             { return [NSNumber numberWithDouble:v];           }
+NSNumber * numberWithFloat           (float v)              { return [NSNumber numberWithFloat:v];            }
+NSNumber * numberWithInt             (signed int v)         { return [NSNumber numberWithInt:v];              }
+NSNumber * numberWithLong            (signed long v)        { return [NSNumber numberWithLong:v];             }
+NSNumber * numberWithLongLong        (signed long long v)   { return [NSNumber numberWithLongLong:v];         }
+NSNumber * numberWithShort           (signed short v)       { return [NSNumber numberWithShort:v];            }
+NSNumber * numberWithUnsignedChar    (unsigned char v)      { return [NSNumber numberWithUnsignedChar:v];     }
+NSNumber * numberWithUnsignedInt     (unsigned int v)       { return [NSNumber numberWithUnsignedInt:v];      }
+NSNumber * numberWithUnsignedLong    (unsigned long v)      { return [NSNumber numberWithUnsignedLong:v];     }
+NSNumber * numberWithUnsignedLongLong(unsigned long long v) { return [NSNumber numberWithUnsignedLongLong:v]; }
+NSNumber * numberWithUnsignedShort   (unsigned short v)     { return [NSNumber numberWithUnsignedShort:v];    }
