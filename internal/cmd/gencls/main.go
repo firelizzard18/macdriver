62703630afd8b9dd27007d7c22803a4efd061176
Ethan Reesor <ethan.reesor@gmail.com>
2021-07-27 19:42:09 -0500

    Skip non-NSObjects

--- internal/cmd/gencls/main.go
+++ internal/cmd/gencls/main.go
@@ -6,8 +6,18 @@ package main
 #define OBJC2_UNAVAILABLE
 #include <objc/objc.h>
 #include <objc/runtime.h>
+#include <Foundation/Foundation.h>
+
+bool isaNSObject(Class cls) {
+	if (!class_getClassMethod(cls, @selector(isSubclassOfClass:))) {
+		return NO;
+	}
+	return [cls isSubclassOfClass:[NSObject class]];
+}
 */
 import "C"
+
+const nameNSObject = "NSObject\x00"
 
 func main() {
 	n := C.objc_getClassList(nil, 0)
@@ -15,6 +25,11 @@ func main() {
 	C.objc_getClassList(&classes[0], n)
 
 	for _, class := range classes[:100] {
-		println(C.GoString(C.class_getName(class)))
+		name := C.GoString(C.class_getName(class))
+		if !C.isaNSObject(class) {
+			continue
+		}
+
+		println(name)
 	}
 }
